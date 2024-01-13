981e5df7e6e0d9367109a50137437b0368e9533c
Braxton Revills <braxtonrr9@gmail.com>
2024-01-12 22:08:44 -0500

    Routing issues attempted fix2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import './App.css'
 import { Home } from './assets/Components/Home'
 import { FieldEscape } from './assets/Components/FieldEscape'
-import {Routes, Route, BrowserRouter} from 'react-router-dom'
+import {Routes, Route,  BrowserRouter} from 'react-router-dom'
 import { AiSwirl } from './assets/Components/AiSwirl'
 import { VibeZone } from './assets/Components/VibeZone'
 import { TheProcess } from './assets/Components/TheProcess'
@@ -12,18 +12,14 @@ import { AlgaeSphere } from './assets/Components/AlgaeSphere'
 function App() {
 
   return (
-    <>
-    <BrowserRouter basename='/visiostudio/'>
-      <Routes>
-        <Route path="/visiostudio/" element={<Home />} />
+    <Routes>
+      <Route path="/visiostudio/" element={<Home />} />
         <Route path="/visiostudio/fieldescape" element={<FieldEscape />} />
         <Route path="/visiostudio/aiswirl" element={<AiSwirl />} />
         <Route path="/visiostudio/vibezone" element={<VibeZone />} />
         <Route path="/visiostudio/theprocess" element={<TheProcess />} />
         <Route path="/visiostudio/algaesphere" element={<AlgaeSphere />} />
       </Routes>
-      </BrowserRouter>
-      </>
   )
 }
 
